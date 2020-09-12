a9ad88fce992dbb46ad5f86e28ea3956bd310328
Siddharth Kundu <sk31395@gmail.com>
2020-09-12 16:36:55 +0530

    added register mutation

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -3,16 +3,33 @@ import { Formik, Form } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/core";
+import { useMutation } from "urql";
 
-interface registerProps {}
+interface registerProps { }
 
-const Register: React.FC<registerProps> = ({}) => {
+const REGISTER_MUTATION = `
+  mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password }) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+`
+
+const Register: React.FC<registerProps> = ({ }) => {
+  const [{ }, register] = useMutation(REGISTER_MUTATION)
   return (
     <Wrapper variant="regular">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values) => {
+          const response = await register(values)
         }}
       >
         {({ isSubmitting }) => (
