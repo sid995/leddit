af53fb33358ee77a58668ec3b9f77bc97fbbbdec
Siddharth Kundu <sk31395@gmail.com>
2020-09-17 05:34:04 +0530

    new logout mutation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -12,6 +12,7 @@ import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 import { EntityManager } from "mikro-orm";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -152,5 +153,23 @@ export class UserResolver {
 		req.session.userId = user.id;
 
 		return { user };
+	}
+
+	@Mutation(() => Boolean)
+	logout(
+		@Ctx() {req, res}: MyContext
+	) {
+		return new Promise(resolve => 
+			req.session.destroy(err => {
+				if(err) {
+					console.log(err)
+					resolve(false)
+					return
+				}
+
+				res.clearCookie(COOKIE_NAME)
+				resolve(true)
+			})
+		)
 	}
 }
